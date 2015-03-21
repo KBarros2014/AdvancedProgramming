7ea4b352def29aea9840fd524f7ebf61e554828c
Barros <katieb784@gmail.com>
2015-03-22 00:44:31 +1300

    Class tiles

---
+++ katie/tiles.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      katie
+#
+# Created:     22/03/2015
+# Copyright:   (c) katie 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class Tiles:
+    Foyer = 0
+    FamilyRoom = 1
+    StorageRoom = 2
+    Bedroom = 3
+    Patio = 4
+    EvilTemple = 5
+    Kitchen = 6
+    Bathroom = 7
+    Garden = 8
+    Graveyard = 9
+    Garage = 10
+    DiningRoom = 11
+    SittingArea = 12
+}
