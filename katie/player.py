58b43bceca36f8f918f7e2e147fea1bc5fbfc28a
Barros <katieb784@gmail.com>
2015-03-22 01:59:06 +1300

    python not java

--- katie/player.py
+++ katie/player.py
@@ -8,18 +8,9 @@
 # Copyright:   (c) katie 2015
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+class Player(object):
 
-class Tile (object):
-    Foyer = 0
-    FamilyRoom = 1
-    StorageRoom = 2
-    Bedroom = 3
-    Patio = 4
-    EvilTemple = 5
-    Kitchen = 6
-    Bathroom = 7
-    Garden = 8
-    Graveyard = 9
-    Garage = 10
-    DiningRoom = 11
-    SittingArea = 12
+
+
+
+}
