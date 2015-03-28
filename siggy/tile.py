a4f305b38055763400926c361a9ff4fab09a28d2
siggyzee <comteckbird@gmail.com>
2015-03-28 16:32:07 +1300

    tile.py

--- siggy/tile.py
+++ siggy/tile.py
@@ -1,6 +1,7 @@
 __author__ = 'siggyzee'
 __name__ = "tile"
 __package__ = "gimp_game"
+
 class Tile:
     def __init__(self, name = "", north = 0, east = 0, south = 0, west = 0):
         self.type = False
